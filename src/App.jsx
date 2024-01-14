22e2c65d46eeff0371b6d504c81f33ab89a5b92b
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-01-14 21:26:53 +0600

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import StudentsTable from "./components/StudentsTable";
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <StudentsTable />
+      <Footer />
     </div>
   );
 };
